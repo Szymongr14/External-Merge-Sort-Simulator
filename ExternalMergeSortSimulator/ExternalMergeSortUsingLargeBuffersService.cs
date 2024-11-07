b6cea86eff8870ba1b7a8179dda4aff4cb16cc9c
Szymongr14 <szymongr14@gmail.com>
2024-11-07 20:33:39 +0100

    split CreateRuns method into multiple smaller ones

--- ExternalMergeSortSimulator/ExternalMergeSortUsingLargeBuffersService.cs
+++ ExternalMergeSortSimulator/ExternalMergeSortUsingLargeBuffersService.cs
@@ -1,4 +1,5 @@
 using ExternalMergeSortSimulator.Interfaces;
+using ExternalMergeSortSimulator.Models;
 using MemoryPageAccessSimulator.Interfaces;
 using MemoryPageAccessSimulator.Models;
 using Microsoft.Extensions.Logging;
@@ -26,44 +27,81 @@ public class ExternalMergeSortUsingLargeBuffersService
         var records = _datasetInputStrategy.GetRecords();
         _memoryManagerService.WriteInitialRecordsToBinaryFile(records, $"Disk/{InitialRecordsFile}");
         CreateRuns($"Disk/{InitialRecordsFile}");
+        Merge("Disk");
     }
-    
+
+    private void Merge(string diskDir)
+    {
+        var mergeMinHeap = new PriorityQueue<HeapElement, int>();
+        
+        var offsets = new int[_appSettings.RAMSizeInNumberOfPages];
+       
+        
+    }
     private void CreateRuns(string filePath)
     {
-        var offset = 0;
+        var initialRecordsFileOffset = 0;
         var runCounter = 0;
-        var pageHasEmptySlotsFlag = false;
         var maxOffsetForInitialFile = _memoryManagerService.GetMaxPageOffsetForFile(filePath);
-        var readPages = 0;
-        
+
         _logger.LogInformation("Initial Run Generation phase has started...");
 
-        while (offset < maxOffsetForInitialFile)
+        while (initialRecordsFileOffset < maxOffsetForInitialFile)
         {
-            while (!_memoryManagerService.RAMIsFull() && !pageHasEmptySlotsFlag)
-            {
-                var page = _memoryManagerService.ReadPageFromTape(filePath, offset++);
-                _memoryManagerService.InsertPageIntoRAM(page!);
-                if (page!.GetNumberOfRecords() < _appSettings.PageSizeInNumberOfRecords) pageHasEmptySlotsFlag = true;
-                readPages++;
-            }
-            SortRecordsInRAM();
-            if(_appSettings.LogLevel == "Detailed") PrintRun(runCounter);
-            var pageIndex = 0;
-            while (pageIndex < readPages)
-            {
-                var page = _memoryManagerService.GetLastPageFromRAM();
-                _memoryManagerService.RemoveLastPageFromRAM();
-                _memoryManagerService.WritePageToTape(page, $"Disk/run_{runCounter}.bin");
-                pageIndex++;
-            }
-
+            var (readPages, newOffset) = LoadPagesIntoRAM(filePath, initialRecordsFileOffset);
+            initialRecordsFileOffset = newOffset;
+            SortAndSaveRunToDisk(runCounter, readPages);
             runCounter++;
-            readPages = 0;
         }
 
+        LogSummary(runCounter);
+    }
+
+    private (int readPages, int newOffset) LoadPagesIntoRAM(string filePath, int offset)
+    {
+        var readPages = 0;
+        var pageHasEmptySlotsFlag = false;
+
+        while (!_memoryManagerService.RAMIsFull() && !pageHasEmptySlotsFlag && offset < _memoryManagerService.GetMaxPageOffsetForFile(filePath))
+        {
+            var page = _memoryManagerService.ReadPageFromTape(filePath, offset++);
+            _memoryManagerService.InsertPageIntoRAM(page!);
+
+            if (page!.GetNumberOfRecords() < _appSettings.PageSizeInNumberOfRecords)
+            {
+                pageHasEmptySlotsFlag = true;
+            }
+
+            readPages++;
+        }
+
+        return (readPages, offset);
+    }
+
+    private void SortAndSaveRunToDisk(int runCounter, int readPages)
+    {
+        SortRecordsInRAM();
+        var pageIndex = 0;
+        
+        if (_appSettings.LogLevel == "Detailed")
+        {
+            PrintRun(runCounter);
+        }
+
+        while (pageIndex < readPages)
+        {
+            var page = _memoryManagerService.GetLastPageFromRAM();
+            _memoryManagerService.RemoveLastPageFromRAM();
+            _memoryManagerService.WritePageToTape(page, $"Disk/run_{runCounter}.bin");
+
+            pageIndex++;
+        }
+    }
+
+    private void LogSummary(int runCounter)
+    {
         var (currentTotalWrites, currentTotalReads) = _memoryManagerService.GetTotalReadsAndWrites();
-        _logger.LogInformation("Initial Run Generation phase has ended... {} runs was created and sorted! Total writes: {}, Total reads: {}", runCounter, currentTotalWrites, currentTotalReads);
+        _logger.LogInformation("Initial Run Generation phase has ended... {runCounter} runs were created and sorted! Total writes: {totalWrites}, Total reads: {totalReads}", runCounter, currentTotalWrites, currentTotalReads);
     }
 
     private void PrintRun(int runNumber)
@@ -76,6 +114,7 @@ public class ExternalMergeSortUsingLargeBuffersService
             if (i == 10)
             {
                 Console.WriteLine("                            ...");
+                Console.WriteLine($"                     ({records.Count - i} more records)");
                 break;
             }
         }
---
+++ ExternalMergeSortSimulator/Models/HeapElement.cs
@@ -0,0 +1,3 @@
+namespace ExternalMergeSortSimulator.Models;
+
+public record HeapElement();
